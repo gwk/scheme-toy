fbcf1a904656c61801cbb7b3fb84746959eacba9
gwk <george.w.king@gmail.com>
2013-05-07 13:12:20 -0400

    tests

---
+++ tests/add1.scm
@@ -0,0 +1,5 @@
+(define add1
+   (lambda (x)
+      (+ 1 x)))
+
+(test (add1 2) 3)
--- tests/fib.scm
+++ tests/fib.scm
@@ -1,14 +1,29 @@
-; comment
-(define fibonacci
+; simplest fib implementation calculates one step larger than desired
+(define fib
+  (lambda (n)
+    (define fib_rec
+      (lambda (a b count)
+        (if (eq? count n)
+          a
+          (fib_rec b (+ a b) (+ count 1)))))
+    (fib_rec 0 1 0)))
+
+; more complex version checks zero case specially so that the recursion can return the larger of the two arguments.
+(define fib1
   (lambda (n)
     (if (eq? n 0)
       0
       (begin
-        (define fib
+        (define fib_rec
           (lambda (a b count)
             (if (eq? count n)
               b
-              (fib b (+ a b) (+ count 1)))))
-        (fib 0 1 1)))))
+              (fib_rec b (+ a b) (+ count 1)))))
+        (fib_rec 0 1 1)))))
 
-(display (fibonacci 10000))
+
+(define fib_1000_exp
+     43466557686937456435688527675040625802564660517371780402481729089536555417949051890403879840079255169295922593080322634775209689623239873322471161642996440906533187938298969649928516003704476137795166849228875)
+
+(test (fib  1000) fib_1000_exp)
+(test (fib1 1000) fib_1000_exp)
